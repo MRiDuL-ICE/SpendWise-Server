c80f620c2fca26dd731560f441c8130d03c5b43c
“MRiDuL-ICE” <abdulwahab2240@gmail.com>
2025-10-22 21:54:40 +0600

    Prisma import issue resolved

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,42 @@
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Express, Request, Response } from 'express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { appConfig } from './config/app.config';
 
+const expressApp = express();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedHandler: Express | null = null;
 
-  app.setGlobalPrefix('api/spend-wise/v1');
+export default async function handler(req: Request, res: Response) {
+  if (!cachedHandler) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
 
-  const config = new DocumentBuilder()
-    .setTitle('SpendWise API')
-    .setDescription('The SpendWise API description')
-    .setVersion('1.0')
-    .addTag('SpendWise')
-    .addBearerAuth(
-      {
-        description: 'Enter JWT token',  // Optional: Custom description
-        type: 'http',
-        in: 'header',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
-      'access-token',  // Name of the auth scheme (used in @ApiBearerAuth)
-    )
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/spend-wise/v1/docs', app, documentFactory);
-  console.log(`server running on port: ${appConfig.port ?? 3000}`);
-  console.log(`swagger link: ${appConfig.apiBaseUrl}/docs`);
+    app.setGlobalPrefix('api/spend-wise/v1');
 
-  await app.listen(process.env.PORT ?? 3000);
+    const config = new DocumentBuilder()
+      .setTitle('SpendWise API')
+      .setDescription('The SpendWise API description')
+      .setVersion('1.0')
+      .addBearerAuth(
+        {
+          description: 'Enter JWT token',
+          type: 'http',
+          in: 'header',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+        'access-token',
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/spend-wise/v1/docs', app, document);
+
+    await app.init();
+    cachedHandler = expressApp;
+  }
+
+  // ✅ Type-safe call
+  return cachedHandler(req, res);
 }
-bootstrap();
